761e2bcbccf01df8a3800a00768482d4a2b85610
Waleed Abuzaid <eng.waleed.abuzaid@gmail.com>
2022-10-17 14:10:20 +0300

    intial

---
+++ RestFul Example/RestFul Example/IRestFulDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using System.ServiceModel.Web;
+
+namespace RestFul_Example
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestFulDemo" in both code and config file together.
+    [ServiceContract]
+    public interface IRestFulDemo
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Xml, UriTemplate = "GetDefaultFormat/{id}")]
+        string GetDefaultFormat(string id);
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "GetJsonFormat/{id}")]
+        string GetJsonFormat(string id);
+    }
+}
---
+++ RestFul Example/RestFul Example/RestFulDemo.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RestFul_Example
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestFulDemo" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestFulDemo.svc or RestFulDemo.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(AddressFilterMode =AddressFilterMode.Any)]
+    public class RestFulDemo : IRestFulDemo
+    {
+        public string GetDefaultFormat(string id) => $"This is XmlFormat {id}";
+
+        public string GetJsonFormat(string id) => $"This is Json Format {id}";
+
+    }
+}
